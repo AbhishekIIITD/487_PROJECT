87914be381aff644296864600a85169bbf5aca01
AbhishekIIITD <abhishek21121@iiitd.ac.in>
2022-12-27 17:38:53 +0530

    changed the ratings

--- components/Home_Page/ProductsCard.jsx
+++ components/Home_Page/ProductsCard.jsx
@@ -1,9 +1,15 @@
-import { Grid } from "@mui/material"
+import { Grid,Rating } from "@mui/material"
+
+
+
 export const ProductsCard = (props) => {
+  console.log(props);
   return (
     <div className=" p-8 align-middle  flex flex-col  justify-center text-center">
     <Grid item xs={3}>
       <img className=" mb-4 h-72 max-md:h-28 " src={"/"+props.id.substr(0,2)+"/"+props.id+".jpeg"} alt="not loading"></img>
+      <Rating name="read-only" value={parseInt(props.rating)} readOnly />
+      <br></br>
       <button className=" border border-blue-700 border-spacing-5 hover:bg-blue-400 p-2 rounded-md w-16" type="submit">BUY</button>
     </Grid></div>
   )
